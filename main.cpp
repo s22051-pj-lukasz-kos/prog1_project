2a44490b15ed8838e3f4d2a502b68a945298d829
s22051-pj-lukasz-kos <s22051-pj-lukasz-kos@pjwstk.edu.pl>
2021-01-21 20:29:14 +0100

    dodanie funkcji czyszczenia ekranu funkcja ustawiania kursora na środku zalążki keyeventa do obsługi sterowania
    To Do:
    w pierwszej kolejności sterowanie kursorem

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@
 
 using namespace std;
 
-char inputChar(char sign);          // wpisywanie wyœwietlanego znaku ASCII
+void inputChar(char &sign);          // wpisywanie wyœwietlanego znaku ASCII
 void screenBufferInfo(HANDLE hOut); // zbieranie informacji o screen buffer
 void startScreen ();                // ekran powitalny
 void clearScreen(HANDLE hOut);      // czyœci ekran
 void tutor();                       // sterowanie wyœwietlane u góry ekranu
-void cursorPosition();              // ustawia pozycjê kursora
-void cursorPositionStart();         // ustawia pozycjê kursora po uruchomieniu
+void cursorPosition(HANDLE hOut);              // ustawia pozycjê kursora
+void cursorPositionStart(HANDLE hOut);         // ustawia pozycjê kursora po uruchomieniu
 void cursorMovement();              // funkcja do poruszania siê kursorem
 
 int main()
@@ -24,14 +24,32 @@ int main()
     setlocale(LC_ALL, "");
 
     HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);   // ³¹cznik z wyjœciem standardowym z konsoli
+    HANDLE hConsoleIn = GetStdHandle(STD_INPUT_HANDLE);   // ³¹cznik z wejœciem standardowym z konsoli
     char letter;                // wyœwietlany znak
 
     SetConsoleTitle("s22051");  // tytu³ okna
     startScreen();              // ekran powitalny
-    letter = inputChar(letter); // wpisywanie znaku
+    inputChar(letter);          // wpisywanie znaku
     clearScreen(hConsoleOut);   // czyszczenie ekranu
+    cursorPositionStart(hConsoleOut);      // ustawia kursor na œrodek ekranu
+    cout << letter;
 
-    screenBufferInfo(hConsoleOut);  // zbieranie informacji o screen buffer
+//    screenBufferInfo(hConsoleOut);  // zbieranie informacji o screen buffer
+
+    INPUT_RECORD eventInputArray[128];
+    DWORD numInput;
+    int escChecker;
+    do {
+        ReadConsoleInput(hConsoleIn, eventInputArray, 128, &numInput);
+        for (int i=0; i<numInput; i++) {
+                if (eventInputArray[i].Event.KeyEvent.bKeyDown) {
+                    escChecker = eventInputArray[i].Event.KeyEvent.wVirtualKeyCode;
+                    cout << "wVirtualKeyCode: " << eventInputArray[i].Event.KeyEvent.wVirtualKeyCode << endl;
+                    cout << "wVirtualScanCode: " << eventInputArray[i].Event.KeyEvent.wVirtualScanCode << endl;
+                }
+           }
+    } while (escChecker != 27); // ESC
+
 
 
 
@@ -39,10 +57,9 @@ int main()
 }
 
 // funkcja do wpisywania i zwracania drukowanego znaku
-char inputChar(char sign) {
+void inputChar(char &sign) {
     cout << "Proszê wpisaæ dowolny znak" << endl;
     cin >> sign;
-    return sign;
 }
 
 // funkcja testowa do testowania GetConsoleScreenBufferInfo()
@@ -81,11 +98,14 @@ void clearScreen(HANDLE hOut) {
     CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;   // zaimplemmentowana zmienna, która otrzyma informacje o screen buffer
     DWORD screenBufferTotalChar;    // sumaryczna liczba znaków screen buffer
     COORD startPoint = {0, 0};      // punkt startowy czyszczenia ekranu
+    DWORD charWritten;              // zwracana wartoœæ. Musia³em j¹ zadeklarowaæ, bo inaczej funkcja FillConsoleOutputCharacter przerywa³¹ dzia³anie programu
 
     GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);    // zaimplementowana zmienna, która zbiera informacje o screen buffer i przekazuje do zmiennej consoleBufferInfo
     screenBufferTotalChar = consoleBufferInfo.dwSize.X * consoleBufferInfo.dwSize.Y;    // opis przy deklaracji zmiennych
 
-    FillConsoleOutputCharacter(hOut, ' ', screenBufferTotalChar, startPoint, NULL); // zaimplementowana funkcja do wype³nienia ekranu wybranym znakiem
+    FillConsoleOutputCharacter(hOut, ' ', screenBufferTotalChar, startPoint, &charWritten); // zaimplementowana funkcja do wype³nienia ekranu wybranym znakiem
+
+    SetConsoleCursorPosition(hOut, startPoint); // resetuje pozycjê kursora na pocz¹tek
 }
 
 // sterowanie wyœwietlane u góry ekranu
@@ -94,13 +114,23 @@ void tutor() {
 }
 
 // ustawia pozycjê kursora
-void cursorPosition() {
+void cursorPosition(HANDLE hOut) {
 
 }
 
-// ustawia pozycjê kursora po uruchomieniu
-void cursorPositionStart() {
+// ustawia pozycjê kursora na œrodek ekranu
+void cursorPositionStart(HANDLE hOut) {
+    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;
+    COORD middleScreen;
 
+    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);
+
+    // obliczam œrodek ekranu w osi X
+    middleScreen.X = (consoleBufferInfo.srWindow.Right - consoleBufferInfo.srWindow.Left) / 2;
+    // obliczam œrodek ekranu w osi Y
+    middleScreen.Y = (consoleBufferInfo.srWindow.Bottom - consoleBufferInfo.srWindow.Top) / 2;
+
+    SetConsoleCursorPosition(hOut, middleScreen);   // zaimplementowana funkcja do ustawiania pozycji kursora
 }
 
 // funkcja do poruszania siê kursorem
