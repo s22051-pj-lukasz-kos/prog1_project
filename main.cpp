f0eff88438fc71c19a7645cea7bde0b8b9570672
s22051-pj-lukasz-kos <s22051-pj-lukasz-kos@pjwstk.edu.pl>
2021-01-23 16:36:05 +0100

    Dodane rysowanie symbolu,  poruszanie się i zmiana wielkości symbolu TO DO: utworzenie osobnej funkcji zarządzającej zmianą wielkości symbolu i ograniczenie poruszania się (ustalenie granic)

--- main.cpp
+++ main.cpp
@@ -1,55 +1,67 @@
 /*
+    Podstaw¹ pod zaimplementowane funkcje z biblioteki windows.h sta³ siê Microsoft'owy dokument o konsoli
+    https://docs.microsoft.com/en-us/windows/console/
+
+
     1. program ma umo¿liwiæ wczytanie rozmiarów na samym pocz¹tku
     2. z biblioteki windows.h pobieramy rozmiar okna
     3. muszê wykombinowac sterowanie:   https://en.cppreference.com/w/cpp/header; input/output library
                                         https://en.cppreference.com/w/cpp/io
+
+    TO DO:
+        1. napisaæ funkcjê do zarz¹dzania wielkoœci¹ znaku (ustaliæ warunki na bazie wielkoœci pola
+        2. napisaæ funkcjê, która ogarniczy poruszanie siê po ekranie (ustali granice)
+
 */
 
 #include <iostream>
-#include <windows.h> // przesuwanie i pobieranie rozmiarów okna
+
+// szereg zaimplementowanych funkcji do zarz¹dzania kursorem, oknem, screen bufforem i klawiatur¹
+#include <windows.h>
 
 using namespace std;
 
-void inputChar(char &sign);          // wpisywanie wyœwietlanego znaku ASCII
+void inputChar(char &sign);         // wpisywanie wyœwietlanego znaku ASCII
 void screenBufferInfo(HANDLE hOut); // zbieranie informacji o screen buffer
 void startScreen ();                // ekran powitalny
 void clearScreen(HANDLE hOut);      // czyœci ekran
 void tutor();                       // sterowanie wyœwietlane u góry ekranu
-void cursorPosition(HANDLE hOut);              // ustawia pozycjê kursora
-void cursorPositionStart(HANDLE hOut);         // ustawia pozycjê kursora po uruchomieniu
-void cursorMovement();              // funkcja do poruszania siê kursorem
+COORD GetCursorPosition(HANDLE hOut);   // zwraca pozycjê kursora
+COORD heightWidthWindow (HANDLE hOut);  // funkcja do wyliczania wysokoœci i szerokoœci buffor screen w oknie
+void cursorPosition(HANDLE hOut);       // ustawia pozycjê kursora
+COORD cursorPositionStart(HANDLE hOut);  // ustawia pozycjê kursora po uruchomieniu
+int keyEvent(HANDLE hIn);               // funkcja zwraca wybrane virtual-key codes (numery klawiszy)
+void eventManager(HANDLE hOut);
+void keyEventManager(HANDLE hOut, bool &run, int vKeyCode, char &sign, int &symbSize);              // funkcja do poruszania siê kursorem
+void drawSymbol (HANDLE hOut, char &sign, int &symbSize, COORD cursorPosition);     // funkcja do rysowania symbolu
+void symbolSizeManager();
 
 int main()
 {
     setlocale(LC_ALL, "");
 
-    HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);   // ³¹cznik z wyjœciem standardowym z konsoli
+    HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);   // ³¹cznik z wyjœciem standardowym z konsoli, z windows.h
     HANDLE hConsoleIn = GetStdHandle(STD_INPUT_HANDLE);   // ³¹cznik z wejœciem standardowym z konsoli
     char letter;                // wyœwietlany znak
+    int symbolSize;             // wielkoœæ wyœwietlanego symbolu
+    COORD middleWindow;         // œrodek okna
+    bool running = true;        // zmienna do przerywania dzia³ania programu
+    // zaimplementowana typ zmiennej do zmiany wygl¹du kursora
+    CONSOLE_CURSOR_INFO conCurInfo = {100, TRUE};
 
-    SetConsoleTitle("s22051");  // tytu³ okna
+    SetConsoleTitle("s22051");  // tytu³ okna, z windows.h
     startScreen();              // ekran powitalny
     inputChar(letter);          // wpisywanie znaku
+    symbolSize = 3;
     clearScreen(hConsoleOut);   // czyszczenie ekranu
-    cursorPositionStart(hConsoleOut);      // ustawia kursor na œrodek ekranu
-    cout << letter;
+    middleWindow = cursorPositionStart(hConsoleOut);      // ustawia kursor na œrodek ekranu
+    SetConsoleCursorInfo(hConsoleOut, &conCurInfo); // z windows.h, zmienia wygl¹d kursora
 
-//    screenBufferInfo(hConsoleOut);  // zbieranie informacji o screen buffer
-
-    INPUT_RECORD eventInputArray[128];
-    DWORD numInput;
-    int escChecker;
+    drawSymbol(hConsoleOut, letter, symbolSize, middleWindow);        // drukowanie symbolu
     do {
-        ReadConsoleInput(hConsoleIn, eventInputArray, 128, &numInput);
-        for (int i=0; i<numInput; i++) {
-                if (eventInputArray[i].Event.KeyEvent.bKeyDown) {
-                    escChecker = eventInputArray[i].Event.KeyEvent.wVirtualKeyCode;
-                    cout << "wVirtualKeyCode: " << eventInputArray[i].Event.KeyEvent.wVirtualKeyCode << endl;
-                    cout << "wVirtualScanCode: " << eventInputArray[i].Event.KeyEvent.wVirtualScanCode << endl;
-                }
-           }
-    } while (escChecker != 27); // ESC
+        keyEventManager(hConsoleOut, running, keyEvent(hConsoleIn), letter, symbolSize);
 
+    } while (running);
 
 
 
@@ -60,6 +72,145 @@ int main()
 void inputChar(char &sign) {
     cout << "Proszê wpisaæ dowolny znak" << endl;
     cin >> sign;
+}
+
+// ekran powitalny
+void startScreen () {
+    cout << "Ekran powitalny.\n" << endl;
+
+}
+
+// czyœci ekran
+void clearScreen(HANDLE hOut) {
+    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;   // zaimplemmentowana zmienna, która otrzyma informacje o screen buffer
+    DWORD screenBufferTotalChar;    // sumaryczna liczba znaków screen buffer
+    COORD startPoint = {0, 0};      // punkt startowy czyszczenia ekranu
+    DWORD charWritten;              // zwracana wartoœæ. Musia³em j¹ zadeklarowaæ, bo inaczej funkcja FillConsoleOutputCharacter przerywa³¹ dzia³anie programu
+
+    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);    // zaimplementowana funkcja, która zbiera informacje o screen buffer i przekazuje do zmiennej consoleBufferInfo
+    screenBufferTotalChar = consoleBufferInfo.dwSize.X * consoleBufferInfo.dwSize.Y;    // opis przy deklaracji zmiennych
+
+    FillConsoleOutputCharacter(hOut, ' ', screenBufferTotalChar, startPoint, &charWritten); // zaimplementowana funkcja do wype³nienia ekranu wybranym znakiem
+}
+
+// sterowanie wyœwietlane u góry ekranu
+void tutor() {
+
+}
+
+// ustawia pozycjê kursora na œrodek ekranu
+COORD cursorPositionStart(HANDLE hOut) {
+    COORD middleScreen;
+
+    // obliczam œrodek ekranu w osi X
+    middleScreen.X = heightWidthWindow(hOut).X / 2;
+    // obliczam œrodek ekranu w osi Y
+    middleScreen.Y = heightWidthWindow(hOut).Y / 2;
+
+    SetConsoleCursorPosition(hOut, middleScreen);   // zaimplementowana funkcja do ustawiania pozycji kursora
+    return middleScreen;
+}
+
+// funkcja do wyliczania wysokoœci i szerokoœci buffor screen w oknie
+COORD heightWidthWindow (HANDLE hOut) {
+    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;   // zaimplemmentowana zmienna, która otrzyma informacje o screen buffer
+    COORD heightWidthWindowBuffer;      // zaimplementowany typ zmiennej, która zwraca wysokoœæ i szerokoœæ screen buffer w oknie
+
+    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);    // zaimplementowana zmienna, która zbiera informacje o screen buffer i przekazuje do zmiennej consoleBufferInfo
+    heightWidthWindowBuffer.X = consoleBufferInfo.srWindow.Right - consoleBufferInfo.srWindow.Left;
+    heightWidthWindowBuffer.Y = consoleBufferInfo.srWindow.Bottom - consoleBufferInfo.srWindow.Top;
+
+    return heightWidthWindowBuffer;
+}
+
+// zwraca pozycjê kursora
+COORD GetCursorPosition(HANDLE hOut) {
+    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;   // zaimplemmentowana zmienna, która otrzyma informacje o screen buffer
+
+    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);    // zaimplementowana zmienna, która zbiera informacje o screen buffer i przekazuje do zmiennej consoleBufferInfo
+    return consoleBufferInfo.dwCursorPosition;
+}
+
+// ustawia pozycjê kursora
+void setCursorPosition(HANDLE hOut, COORD posChange) {
+
+}
+
+// funkcja zwraca virtual-key codes (numery klawiszy)
+int keyEvent(HANDLE hIn) {
+    INPUT_RECORD eventInputArray[128];  // tablica eventów
+    DWORD numInput;     // liczba zarejestrowanych eventów
+    DWORD keyCode;        // zwracany kod
+    ReadConsoleInput(hIn, eventInputArray, 128, &numInput);  // zaimplementowana funkcja
+    for (DWORD i=0; i < numInput; i++) {
+            if (eventInputArray[i].Event.KeyEvent.bKeyDown) {   // gdy klawisz jest wciœniêty (puszczenie klawisza to osobny event)
+                keyCode = eventInputArray[i].Event.KeyEvent.wVirtualKeyCode;
+            }
+    }
+    return keyCode;
+}
+
+// funkcja do poruszania siê kursorem
+void keyEventManager(HANDLE hOut, bool &run, int vKeyCode, char &sign, int &symbSize) {
+    COORD cursorPosition = GetCursorPosition(hOut);
+
+    switch (vKeyCode) {
+        case 37 : {     // left arrow
+            // pierwszy warunek s³u¿y do ustalenia granic
+            cursorPosition.X--;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 38 : {     // up arrow
+            cursorPosition.Y--;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 39 : {     // right arrow
+            cursorPosition.X++;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 40 : {     // down arrow
+            cursorPosition.Y++;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 187 : {    // plus
+            symbSize++;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 189 : {    // minus
+            symbSize--;
+            drawSymbol(hOut, sign, symbSize, cursorPosition);
+            break; }
+        case 27 : {     // ESC
+            run = false;
+            break; }
+    }
+}
+
+// funkcja do rysowania symbolu
+void drawSymbol (HANDLE hOut, char &sign, int &symbSize, COORD cursorPosition) {
+    DWORD written;
+    COORD symbolPartCoord = cursorPosition;
+
+    // czyszczenie ekranu i ustalanie pozycji kursora
+    clearScreen(hOut);
+    // zaimplementowana funkcja z windows.h
+    SetConsoleCursorPosition(hOut, cursorPosition);
+
+    // algorytm do rysowania symbolu
+    for (int i=0; i <= 2*symbSize; i++) {
+        symbolPartCoord.Y = cursorPosition.Y - i;
+        if (i <= symbSize) {
+            symbolPartCoord.X = cursorPosition.X + i;
+        } else {
+            symbolPartCoord.X = cursorPosition.X + (2 * symbSize) % i;
+        }
+        FillConsoleOutputCharacterA(hOut, sign, 1, symbolPartCoord, &written);
+    }
+}
+
+// funkcja do zarz¹dzania wielkoœci¹ symbolu
+void symbolSizeManager() {
+
 }
 
 // funkcja testowa do testowania GetConsoleScreenBufferInfo()
@@ -86,54 +237,4 @@ void screenBufferInfo(HANDLE hOut) {
         cout << "\nczy kontynuowaæ?" << endl;
         cin >> dalej;
     } while (dalej == 't');
-}
-
-// ekran powitalny
-void startScreen () {
-    cout << "Ekran powitalny.\n" << endl;
-}
-
-// czyœci ekran
-void clearScreen(HANDLE hOut) {
-    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;   // zaimplemmentowana zmienna, która otrzyma informacje o screen buffer
-    DWORD screenBufferTotalChar;    // sumaryczna liczba znaków screen buffer
-    COORD startPoint = {0, 0};      // punkt startowy czyszczenia ekranu
-    DWORD charWritten;              // zwracana wartoœæ. Musia³em j¹ zadeklarowaæ, bo inaczej funkcja FillConsoleOutputCharacter przerywa³¹ dzia³anie programu
-
-    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);    // zaimplementowana zmienna, która zbiera informacje o screen buffer i przekazuje do zmiennej consoleBufferInfo
-    screenBufferTotalChar = consoleBufferInfo.dwSize.X * consoleBufferInfo.dwSize.Y;    // opis przy deklaracji zmiennych
-
-    FillConsoleOutputCharacter(hOut, ' ', screenBufferTotalChar, startPoint, &charWritten); // zaimplementowana funkcja do wype³nienia ekranu wybranym znakiem
-
-    SetConsoleCursorPosition(hOut, startPoint); // resetuje pozycjê kursora na pocz¹tek
-}
-
-// sterowanie wyœwietlane u góry ekranu
-void tutor() {
-
-}
-
-// ustawia pozycjê kursora
-void cursorPosition(HANDLE hOut) {
-
-}
-
-// ustawia pozycjê kursora na œrodek ekranu
-void cursorPositionStart(HANDLE hOut) {
-    CONSOLE_SCREEN_BUFFER_INFO consoleBufferInfo;
-    COORD middleScreen;
-
-    GetConsoleScreenBufferInfo(hOut, &consoleBufferInfo);
-
-    // obliczam œrodek ekranu w osi X
-    middleScreen.X = (consoleBufferInfo.srWindow.Right - consoleBufferInfo.srWindow.Left) / 2;
-    // obliczam œrodek ekranu w osi Y
-    middleScreen.Y = (consoleBufferInfo.srWindow.Bottom - consoleBufferInfo.srWindow.Top) / 2;
-
-    SetConsoleCursorPosition(hOut, middleScreen);   // zaimplementowana funkcja do ustawiania pozycji kursora
-}
-
-// funkcja do poruszania siê kursorem
-void cursorMovement() {
-
 }
